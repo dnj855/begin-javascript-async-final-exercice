752db090d13bbd1f52da41219cae615f71a13b80
Cédric Lang-Roth <contact@clangroth.fr>
2024-05-23 23:05:35 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import cors from "@fastify/cors";
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+await fastify.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+});
+
+// Test si le serveur fonctionne
+fastify.get("/", async () => {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
